de8da27dea0b4712de7778dc3b680450f933cbf4
akashUnknow <akash.unknow@gmail.com>
2025-11-15 21:48:27 +0530

    fileServing add

---
+++ src/main/java/com/examapp/config/FileServingConfig.java
@@ -0,0 +1,16 @@
+package com.examapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FileServingConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/"); // root folder of uploads
+    }
+}
